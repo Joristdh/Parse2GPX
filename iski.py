0be20264aee6eccf30258c57496c292a47cf0749
Eco-Gaming <7ecogaming4@gmail.com>
2024-03-24 17:11:05 +0100

    add incremental _number for same dates

--- iski.py
+++ iski.py
@@ -4,12 +4,23 @@ from urllib import request
 from js2py import eval_js
 
 import json
+import os
 
 
 # Use JS to convert timestamp to datestring. The z param allows for manual timezone correction
 def toDate(e, z = 2):
     date = eval_js(f'new Date({e})')
     return f'{date.getHours()-z:02}:{date.getMinutes():02}:{date.getSeconds():02}'
+
+# If a file with the same name already exists, add _2, _3, etc. 
+def get_unique_filename(filename):
+    base, ext = os.path.splitext(filename)
+    count = 2
+    new_filename = filename
+    while os.path.exists(new_filename):
+        new_filename = f"{base}_{count}{ext}"
+        count += 1
+    return new_filename
 
 # The base url for iSki shares
 base = 'https://share.iski.cc/shares/share_iski/tracks/'
@@ -22,8 +33,12 @@ for id in argv[1:]:
     # Load and serialize data points
     data = json.loads(json.loads(json.dumps(request.urlopen(base + id + '/geometry.json').read().decode('utf-8'))))
 
+    # Check if GPX file already exists
+    gpx_filename = f'{date}.gpx'
+    gpx_filename = get_unique_filename(gpx_filename)
+
     # Create GPX file template
-    with open(f'{date}.gpx', 'w') as file:
+    with open(gpx_filename, 'w') as file:
         file.write('<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>\n')
         file.write('<gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd" version="1.1" creator="gpx.py">\n')
         file.write('\t<trk>\n')
